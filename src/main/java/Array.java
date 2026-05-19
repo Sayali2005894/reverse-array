e8aacad7f7abc89c5ff2c3f9f15390a0f5785fa7
Sayali2005894 <sayalinavkar@gmail.com>
2026-05-19 12:39:37 +0530

    bianary search

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,18 +1,23 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
-class Array
+class Binary_Search
 {
     public static void main(String[] args)
 
     {
+
+        Scanner sc=new Scanner(System.in);
         int arr[]={374,65,1,6576,76,56,4,6,7,44,7678,97809};
+        int j;
+        Arrays.sort(arr);
+        System.out.println("search number");
+                int k=sc.nextInt();
 
-        for(int i=0;i<11;i++)
-        {
-            Arrays.sort(arr);
-            System.out.println("ascending:  "+arr[i]);
+           j=  Arrays.binarySearch(arr,k);
 
-        }
 
+
+        System.out.println("ascending:  "+j);
     }
 }
